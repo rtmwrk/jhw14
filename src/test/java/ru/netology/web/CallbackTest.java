60c6c33acd84ef05dd53ea4c0050eb4b54e65527
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-06-11 23:57:46 +0300

    Test failed -> error phone client in first test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -44,9 +44,9 @@ class CallbackTest {
         // Получаем массив элементов формы с именем класса "input__control"
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         // 1-й такой элемент - поле ввода фамилии и имени пользователя, заполняем его
-        elements.get(0).sendKeys("%%Иванов Петр");
+        elements.get(0).sendKeys("Иванов Петр");
         // 2-й элемент - поле ввода номера телефона
-        elements.get(1).sendKeys("+79181234567");
+        elements.get(1).sendKeys("+791812345678");
         // 3-й элемент - чекбокс соглашения с условиями, ставим галочку
         driver.findElement(By.className("checkbox__box")).click();
         // 4-й элемент - кнопка отправки формы, нажимаем ее
